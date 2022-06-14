5cf1f0f3b25f8cc34dc57ea2fcc83047539e37e7
Onyx Valencia <200004@utags.edu.mx>
2022-06-14 18:26:14 -0500

    feat: Adding array map

---
+++ index.js
@@ -0,0 +1,10 @@
+//map//
+var numbers = [1, 2, 3, 4];
+var doubles = numbers.map(function(x) {
+   return x * 2;
+});
+
+var numbers = [2, 4, 6];
+var roots = numbers.map(function(num) {
+    return Math.sqrt(num);
+});
